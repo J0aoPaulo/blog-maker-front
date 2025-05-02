8f8780d543908df9cdfe2dfbf02ac737ea927a9d
J0aoPaulo <contato.joaopaulodeveloper@gmail.com>
2025-05-01 22:23:47 -0300

    refactor: better post list

--- src/app/features/posts/pages/post-list/post-list.component.ts
+++ src/app/features/posts/pages/post-list/post-list.component.ts
@@ -3,11 +3,14 @@ import { CommonModule }      from '@angular/common';
 import { Observable }        from 'rxjs';
 import { PostService }       from '../../../../core/services/post.service';
 import { PostResponse }      from '../../../../core/models/post-reponse.model';
+import { Router } from 'express';
+import { RouterModule } from '@angular/router';
+import { PostCardComponent } from '../../components/post-card/post-card.component';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, PostCardComponent],
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
